02a2a5e019d94d936491da5cdf8e5637f0027c1d
Hitarth Solanki <78678265+hitarthsolanki@users.noreply.github.com>
2021-05-17 14:57:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,24 @@ difference = 0;
 rightWristX = 0;
 leftWristX = 0;
 
-  function setup() {
-  video = createCapture(VIDEO);
-  video.size(550, 500);
+ function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
 
-  canvas = createCanvas(550, 550);
-  canvas.position(560,150);
-
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
+    canvas = createCanvas(550,550);
+    canvas.position(600,250);
+      
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose' , gotPoses);
 }
-
+function draw() {
+    background('#FF0000');
+} 
 function modelLoaded() {
-  console.log('PoseNet Is Initialized!');
-}
-
+    console.log('PoseNet Is Initialized!');
+  }
+  function gotPoses(results) {
+      if(results.length > 0) {
+          console.log(results);
+      }
+  }
