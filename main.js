2c4e3799f551de307853c28264d048f772954062
Hitarth Solanki <78678265+hitarthsolanki@users.noreply.github.com>
2021-05-20 16:06:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,24 +2,33 @@ difference = 0;
 rightWristX = 0;
 leftWristX = 0;
 
- function setup() {
-    video = createCapture(VIDEO);
-    video.size(550,500);
+function setup() {
+  video = createCapture(VIDEO);
+  video.size(550,500);
 
-    canvas = createCanvas(550,550);
-    canvas.position(600,250);
-      
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose' , gotPoses);
+  canvas = createCanvas(550,550);
+  canvas.position(560,150);
+    
+  poseNet = ml5.poseNet(video, modelLoaded); 
+  poseNet.on('pose' , gotPoses);
 }
 function draw() {
-    background('#FF0000');
+  document.getElementById("font_size").innerHTML = "Side of the square will be =" + difference;
+  background('#FF0000');
+  fill('#FFC0CB');
+  stroke('#FFC0CB');
+  square(leftWristX,rightWristX,difference);
 } 
 function modelLoaded() {
-    console.log('PoseNet Is Initialized!');
-  }
-  function gotPoses(results) {
-      if(results.length > 0) {
-          console.log(results);
-      }
-  }
+  console.log('PoseNet Is Initialized!');
+}
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.rightWristX.x;
+        rightWristX = results[0].pose.leftWristX.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX " + leftWristX +"rightWristX " + rightWristX + "difference" + difference );
+    }
+}
+
